8e5e3d64817f1b97c2229101d57a7049d6c21f27
edervs <eedervs.97@gmail.com>
2018-09-05 12:15:50 -0500

    Data Structures | Trees | BinarySearchTreeNode and BinarySearchTree insert

---
+++ Python/Data Structures/Trees/BinarySearchTree.py
@@ -0,0 +1,53 @@
+""" Binary Search Tree """
+
+from . import BinarySearchTreeNode
+
+class BinarySearchTree:
+    """
+        Elemental Binary Search Tree
+
+        Attributes
+        ----------
+        root=None: BinarySearchTreeNode
+            Root Node
+
+        Methods
+        -------
+    """
+
+    def __init__(self, root=None):
+        """
+            Binary Search Tree Constructor
+            
+            Parameters
+            ----------
+            root=None: BinarySearchTreeNode
+                Root Node
+        """
+        self.root = root
+
+    def insert(self, element):
+        new_node = BinarySearchTreeNode(element)
+
+        # When there is no root in the tree
+        if self.root == None:
+            self.root = new_node
+            return None
+
+        # Go to a leaf
+        parent_node = None
+        actual_node = self.root
+        while actual_node != None:
+            parent_node = actual_node
+            if actual_node.getElement() <= element:
+                actual_node = actual_node.getLeft()
+            else:
+                actual_node = actual_node.getRight()
+
+        # We are already in a Leaf
+        new_node.setParent(parent_node)
+        if parent_node.getElement() <= element:
+            parent_node.setLeft(new_node)
+        else:
+            parent_node.setRight(new_node)
+
---
+++ Python/Data Structures/Trees/BinarySearchTreeNode.py
@@ -0,0 +1,182 @@
+""" Binary Search Tree Node """
+
+class BinarySearchTreeNode:
+    """
+        Elemental Node in a Binary Search Tree
+
+        Attributes
+        ----------
+        element: any type with comparable function
+        parent: BinarySearchTreeNode
+        left: BinarySearchTreeNode
+        right: BinarySearchTreeNode
+
+        Methods
+        -------
+        hasParent()
+            returns True if there is a node parent, False otherwise
+
+        def hasLeft()
+            returns True if this node has a left node, False otherwise
+
+        def hasRight()
+            returns True if this node has a right node, False otherwise
+    """
+    
+    def __init__(self, element, parent=None, left=None, right=None):
+        """
+            Node Constructor
+            
+            Parameters
+            ----------
+            element : any type with comparable function
+                Node element
+
+            parent=None : BinarySearchTreeNode
+                Node parent
+
+            left=None : BinarySearchTreeNode
+                Node in left
+
+            right=None : BinarySearchTreeNode
+                Node in right
+        """
+        self.element = element
+        self.parent = parent
+        self.left = left
+        self.right = right
+        
+    def __str__(self):
+        """
+            Returns a String representation of this node
+        """
+        return 'BinarySearchTreeNode(' + str(self.element) + ')'
+
+    def __repr__(self):
+        """
+            Returns a String representation of this node
+        """
+        return str(self)
+
+    # All getters and setters
+    def getElement(self):
+        """
+            Get the node element
+            
+            Returns
+            -------
+            element
+                node element
+        """
+        return self.element
+
+    def setElement(self, element):
+        """
+            Set the node element
+            
+            Parameters
+            ----------
+            element : any type with comparable function
+                node element
+        """
+        self.element = element
+
+    def getParent(self):
+        """
+            Get the node element
+            
+            Returns
+            -------
+            parent : BinarySearchTreeNode
+                Node parent
+        """
+        return self.parent
+
+    def setParent(self, parent):
+        """
+            Set the parent node
+            
+            Parameters
+            ----------
+            parent : BinarySearchTreeNode
+                Parent node
+        """
+        self.parent = parent
+
+    def getLeft(self):
+        """
+            Get the node in left
+            
+            Returns
+            -------
+            element : BinarySearchTreeNode
+                Node in left
+        """
+        return self.left
+
+    def setLeft(self, left):
+        """
+            Set the node in right
+            
+            Parameters
+            ----------
+            left : BinarySearchTreeNode
+                Left node
+        """
+        self.left = left
+
+    def getRight(self):
+        """
+            Get the node in right
+            
+            Returns
+            -------
+            right : BinarySearchTreeNode
+                Node in right
+        """
+        return self.right
+
+    def setRight(self, right):
+        """
+            Set the node in right
+            
+            Parameters
+            ----------
+            right : BinarySearchTreeNode
+                Right node
+        """
+        self.right = right
+
+    def hasParent(self):
+        """
+            Check if there is a parent
+            
+            Returns
+            -------
+            Boolean
+                True if this node has a parent, False otherwise
+        """
+        return self.parent == None
+
+    def hasLeft(self):
+        """
+            Check if there is a left node
+            
+            Returns
+            -------
+            Boolean
+                True if this node has a left node, False otherwise
+        """
+        return self.left == None
+
+    def hasRight(self):
+        """
+            Check if there is a right node
+            
+            Returns
+            -------
+            Boolean
+                True if this node has a right node, False otherwise
+        """
+        return self.right == None
+
