b4cf79ebd9dd09d223e747bd1923f8de0a408f4f
Miguel Concha <wormb0t@Miguels-MacBook-Pro.local>
2018-09-13 17:47:05 -0500

    Added implementation of doubly-linked lists.

--- Python/Data Structures/Heaps/Heap.py
+++ Python/Data Structures/Heaps/Heap.py
@@ -1,113 +1,123 @@
-""" Heap """
+""" Min and Max Heaps """
+
+from HeapNode import HeapNode
 
 class Heap:
 
-    def __init__(self, isMinHeap = True, L = []):
-        self.isMinHeap = isMinHeap
-        self.tree = L
+    def __init__(self, is_min_heap = True, L = []):
+        self.is_min_heap = is_min_heap
+        self.tree = []
         if L != []:
-            self._heapifyDown()
+            for x in L:
+                node_to_insert = HeapNode(x, len(self.tree))
+                self.tree.append(node_to_insert)
+            self._heapify_down()
 
-    def _getLeftChildIndex(self, parentIndex):
-        return 2 * parentIndex + 1
+    def _swap(self, node_a, node_b):
+        i = node_a.index
+        j = node_b.index
+        self.tree[i].element, self.tree[j].element = self.tree[j].element, self.tree[i].element
 
-    def _getRightChildIndex(self, parentIndex):
-        return 2 * parentIndex + 2
+    def _is_valid_index(self, index):
+        return index >= 0 and index < len(self.tree) and self.tree[index] != None
 
-    def _getParentIndex(self, childIndex):
-        return (childIndex - 1) // 2
+    def _has_left_child(self, node):
+        return self._is_valid_index(node.get_left_child_index())
 
-    def _hasLeftChild(self, index):
-        return self._getLeftChildIndex(index) < len(self.tree)
+    def _has_right_child(self, node):
+        return self._is_valid_index(node.get_right_child_index())
 
-    def _hasRightChild(self, index):
-        return self._getRightChildIndex(index) < len(self.tree)
+    def _has_parent(self, node):
+        return self._is_valid_index(node.get_parent_index())
 
-    def _hasParent(self, index):
-        return self._getParentIndex(index) >= 0
+    def _get_left_child(self, node):
+        if self._has_left_child(node):
+            return self.tree[node.get_left_child_index()]
 
-    def _getLeftChild(self, index):
-        return self.tree[self._getLeftChildIndex(index)]
+    def _get_right_child(self, node):
+        if self._has_right_child(node):
+            return self.tree[node.get_right_child_index()]
 
-    def _getRightChild(self, index):
-        return self.tree[self._getRightChildIndex(index)]
+    def _get_parent(self, node):
+        if self._has_parent(node):
+            return self.tree[node.get_parent_index()]
 
-    def _getParent(self, index):
-        return self.tree[self._getParentIndex(index)]
-
-    def _swap(self, i, j):
-        self.tree[i], self.tree[j] = self.tree[j], self.tree[i]
-
-    def _isValidIndex(self, index):
-        return index >= 0 and index < len(self.tree) and self.tree[index] is not None
-
-    def _heapifyUp(self):
-        index = len(self.tree) - 1
-        if self.isMinHeap:
-            while(self._hasParent(index) and self._getParent(index) > self.tree[index]):
-                self._swap(self._getParentIndex(index), index)
-                index = self._getParentIndex(index)
-        else:
-            while(self._hasParent(index) and self._getParent(index) < self.tree[index]):
-                self._swap(self._getParentIndex(index), index)
-                index = self._getParentIndex(index)
-
-    def _heapifyDown(self):
-        index = 0
-        if self.isMinHeap:
-            while(self._hasLeftChild(index)):
-                smallerChildIndex = self._getLeftChildIndex(index)
-                if(self._hasRightChild(index) and self._getRightChild(index) < self._getLeftChild(index)):
-                    smallerChildIndex = self._getRightChildIndex(index)
-                if(self.tree[index] < self.tree[smallerChildIndex]):
-                    break
-                else:
-                    self._swap(index, smallerChildIndex)
-                    index = smallerChildIndex
-        else:
-            while(self._hasLeftChild(index)):
-                largerChildIndex = self._getLeftChildIndex(index)
-                if(self._hasRightChild(index) and self._getRightChild(index) > self._getLeftChild(index)):
-                    largerChildIndex = self._getRightChildIndex(index)
-                if(self.tree[index] > self.tree[largerChildIndex]):
-                    break
-                else:
-                    self._swap(index, largerChildIndex)
-                    index = largerChildIndex
-
-    def getElements(self):
+    def get_list_nodes(self):
         return self.tree
 
-    def numberOfElements(self):
+    def __str__(self):
+        return str(self.get_list_nodes())
+
+    def __repr__(self):
+        return str(self.get_list_nodes())
+
+    def get_number_nodes(self):
         return len(self.tree)
 
-    def isEmpty(self):
-        return self.numberOfElements() == 0
+    def is_empty(self):
+        return self.get_number_nodes == 0
 
     def peek(self):
-        return self.tree[0] if not self.isEmpty() else None
+        return self.tree[0] if not self.is_empty() else None
 
     def insert(self, element):
-        self.tree.append(element)
-        self._heapifyUp()
- 
-    def popHeap(self):
-        if self.isEmpty():
+        new_node = HeapNode(element, len(self.tree))
+        self.tree.append(new_node)
+        self._heapify_up(new_node)
+
+    def remove(self):
+        if self.is_empty():
             return None
         else:
-            element = self.tree[0]
-            self.tree[0] = self.tree[-1]
-            self.tree.pop()
-            self._heapifyDown()
-            return element
+            self._swap(self.tree[0], self.tree[-1])
+            removed_node = self.tree.pop()
+            self._heapify_down()
+            return removed_node.element
 
     def search(self, element):
-        for e in self.tree:
-            if e is None:
+        for node in self.tree:
+            if node == None:
                 return False
-            if e is element:
-                return True
+            if node.element == element:
+                return node
         return False
 
     def get(self, index):
-        return self.tree[index] if self._isValidIndex(index) else None
+        return self.tree[index] if self._is_valid_index(index) else None
+
+    def _heapify_up(self, node):
+        current_node = node
+        if self.is_min_heap:
+            while(self._has_parent(current_node) and self._get_parent(current_node).element > current_node.element):
+                self._swap(self._get_parent(current_node), current_node)
+                current_node = self._get_parent(current_node)
+        else:
+            while(self._has_parent(current_node) and self._get_parent(current_node).element < current_node.element):
+                self._swap(self._get_parent(current_node), current_node)
+                current_node = self._get_parent(current_node)
+    
+    def _heapify_down(self):
+        current_node = self.tree[0]
+        if self.is_min_heap:
+            while(self._has_left_child(current_node)):
+                smaller_child = self._get_left_child(current_node)
+                if(self._has_right_child(current_node) and self._get_right_child(current_node).element < smaller_child.element):
+                    smaller_child = self._get_right_child(current_node)
+                if(current_node.element > smaller_child.element):
+                    curr = smaller_child
+                    self._swap(current_node, smaller_child)
+                    current_node = curr
+                    print("current node: ", current_node)
+                    print(self._has_left_child(current_node))
+                else:
+                    break
+        else:
+            while(self._has_left_child(current_node)):
+                larger_child = self._get_left_child(current_node)
+                if(self._has_right_child(current_node) and self._get_right_child(current_node).element > larger_child.element):
+                    larger_child = self._get_right_child(current_node)
+                if(current_node.element < larger_child.element):
+                    self._swap(current_node, larger_child)
+                    current_node = larger_child
+                else:
+                    break
