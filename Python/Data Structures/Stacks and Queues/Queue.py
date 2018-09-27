5fcfa44fff9c8d48da86bf4bc43ebe5bd671a205
Miguel Concha <wormb0t@Miguels-MacBook-Pro.local>
2018-09-27 12:22:57 -0500

    Commenting Queues and Stacks.

--- Python/Data Structures/Stacks and Queues/Queue.py
+++ Python/Data Structures/Stacks and Queues/Queue.py
@@ -3,8 +3,23 @@
 from RestrictedList import RestrictedList
 
 class Queue(RestrictedList):
+	"""
+	In qeueues, the first element that was added is the first
+	one to be removed.
+
+	This class inherits from the RestrictedList class
+	which already implements the removal method.
+	"""
 
     def insert(self, element):
+    	"""
+    	Inserts an element into the Stack.
+    	Since the element removed in the RestrictedList is the one
+    	in the head of the list, then we need to insert the elements 
+    	in the tail so that we respect the desired order of removal.
+
+    	:param element: The element to be inserted.
+    	"""
         if element == None:
             return None
         new_node = self.Node(element)
--- Python/Data Structures/Stacks and Queues/Stack.py
+++ Python/Data Structures/Stacks and Queues/Stack.py
@@ -3,8 +3,22 @@
 from RestrictedList import RestrictedList
 
 class Stack(RestrictedList):
+	"""
+	In stacks, the last element that was added is the first
+	one to be removed.
 
+	This class inherits from the RestrictedList class
+	which already implements the removal method.
+	"""
     def insert(self, element):
+    	"""
+    	Inserts an element into the Stack.
+    	Since the element removed in the RestrictedList is the one
+    	in the head of the list, then we need to insert those elements
+    	there.
+
+    	:param element: The element to be inserted.
+    	"""
         if element == None:
             return None
         new_node = self.Node(element)
