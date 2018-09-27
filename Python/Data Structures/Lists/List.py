477170fd39526e1dabf040e057d1359fa52d3d77
Miguel Concha <wormb0t@Miguels-MacBook-Pro.local>
2018-09-27 11:12:14 -0500

    Commenting Lists.

--- Python/Data Structures/Lists/List.py
+++ Python/Data Structures/Lists/List.py
@@ -1,20 +1,84 @@
 """ List (Doubly-Linked list) """
 
 class List:
+    """
+        Doubly-Linked List implementation
 
+        Attributes
+        ----------
+        head: Node
+            Head Node
+
+        tail: Node
+            Tail Node
+
+        size: int
+            Number of elements in the list.
+
+        Methods
+        -------
+        get_length()
+        is_empty()
+        insert(element)
+            insert_back(element)
+            insert_front(element)
+        remove(element)
+            remove_first()
+            remove_last()
+        search(element)
+        contains(element)
+        reverse()
+        clear()
+        get_first()
+        get_last()
+        get(index)
+        index(element)
+        copy()
+    """
     class Node:
-        
+        """
+            Node that contains the element.
+
+            Attributes
+            ----------
+            element: element
+                The key value the node holds.
+            previous: Node
+                A reference to the previous node in the list.
+            next: Node
+                A reference to the next node in the list.
+
+        """
         def __init__(self, element):
+            """
+            Node constructor.
+
+            Parameters
+            ----------
+            element: element
+                The node element. 
+            """
             self.element = element
             self.previous = None
             self.next = None
 
     def __init__(self):
+        """
+        List constructor.
+        """
         self.head = None
         self.tail = None
         self.size = 0
 
     def __str__(self):
+        """
+        Returns the string representation of the list.
+
+        Traverses all the nodes, concatenating in each step the string
+        representation of the element contained in the node.
+        :return: The string representation.
+        :rtype: string
+        """
         current_node = self.head
         list_string = "["
         while current_node != None:
@@ -26,15 +90,38 @@ class List:
         return list_string
 
     def __repr__(self):
+        """
+        Returns the string representation of the list.
+
+        :return: The string representation.
+        :rtype: string
+        """
         return str(self)
 
     def get_length(self):
+        """
+        Returns the number of elements in the list.
+
+        :return: The size of the list.
+        :rtype: int
+        """
         return self.size
 
     def is_empty(self):
+        """
+        Checks whether or not the list has no elements.
+
+        :return: True is it has no element; false otherwise.
+        :rtype: boolean
+        """
         return self.head == None
 
     def insert(self, element):
+        """
+        Inserts a new node in the list, with the element specified as parameter.
+
+        :param element: The element to be inserted in the list.
+        """
         if element == None:
             return
         new_node = self.Node(element)
@@ -48,9 +135,19 @@ class List:
         self.size += 1
 
     def insert_back(self, element):
+        """
+        Inserts the given element at the back of the list.
+
+        :param element: The element to be inserted.
+        """
         self.insert(element)
 
     def insert_front(self, element):
+        """
+        Inserts the given element at the front of the list.
+
+        :param element: The element to be inserted.
+        """
         if element == None:
             return
         new_node = self.Node(element)
@@ -64,6 +161,11 @@ class List:
         self.size += 1
 
     def remove(self, element):
+        """
+        Deletes an element from the list. Decrements the size of the list after the operation.
+
+        :param element: The element which we wish to remove.
+        """
         removed_node = self.search(element)
         if removed_node == None:
             return
@@ -82,6 +184,12 @@ class List:
         self.size -= 1
 
     def remove_first(self):
+        """
+        Deletes the element that is held at the head of the list. Decrements the size of the list
+        after the operation.
+
+        :param element: The element which we wish to remove.
+        """
         if self.is_empty():
             return
         removed_element = self.head.element
@@ -94,6 +202,13 @@ class List:
         return removed_element
 
     def remove_last(self):
+        """
+        Deletes the element that is held at the tail of the list. Decrements the size of the list
+        after the operation.
+
+        :param element: The element which we wish to remove.
+        """
+
         if self.is_empty():
             return
         removed_element = self.tail.element
@@ -106,6 +221,13 @@ class List:
         return removed_element
 
     def search(self, element):
+        """
+        Traverses the list, looking for the specified element.
+
+        :param element: The element we are looking for.
+        :return: None if the element is not found; the element of the node otherwise.
+        :rtype: element
+        """
         result = None
         current_node = self.head
         while current_node != None:
@@ -116,9 +238,23 @@ class List:
         return result.element
 
     def contains(self, element):
+        """
+        Verifies the result of the search query with the given element as parameter.
+
+        :param element: The element which we wish to verify is it is in the list.
+        :return: True is the list contains the element in one of its nodes; False otherwise.
+        :rtype: boolean
+        """
         return self.search(element) != None
 
     def reverse(self):
+        """
+        Reverses the given list so that the elements at the back will now be at the front and
+        viceversa.
+
+        :return: The resulting list after the operation.
+        :rtype: List
+        """
         reversed_list = List()
         current_node = self.head
         while current_node != None:
@@ -127,20 +263,42 @@ class List:
         return reversed_list
 
     def clear(self):
+        """
+        Removes all the elements from the list.
+        """
         while self.size != 0:
             self.remove_last()
 
     def get_first(self):
+        """
+        Returns the first element in the list.
+
+        :return: The element held at the head of the list.
+        :rtype: element
+        """
         if self.is_empty():
             return None
         return self.head.element
 
     def get_last(self):
+        """
+        Returns the last element in the list.
+
+        :return: The element held at the tail of the list.
+        :rtype: element
+        """
         if self.is_empty():
             return None
         return self.tail.element
 
     def get(self, index):
+        """
+        Returns the element that is held at the node at the given index in the list.
+
+        :param index: The query index.
+        :return: The element at the given position.
+        :rype: element
+        """
         if index < 0 or index >= self.size:
             return None
         current_node = self.head
@@ -151,6 +309,13 @@ class List:
         return current_node.element
 
     def index(self, element):
+        """
+        Returns the index of the first occurence of the given element.
+
+        :param element: The element whose index we are looking for.
+        :return: The index of the element.
+        :rtype: int
+        """
         current_node = self.head
         count = 0
         while current_node != None:
@@ -161,9 +326,23 @@ class List:
         return -1
 
     def copy(self):
+        """
+        Copies a list.
+
+        :return: A copy of the list.
+        :rtype: List
+        """
         return self._copy_aux(List(), self.head)
 
     def _copy_aux(self, L, node):
+        """
+        Auxiliary method for copying a list.
+
+        :param L: The list in which the current list will be copied.
+        :param node: The node to be inserted.
+        :return: The list resulting from inserting the element to L.
+        :rtype: List
+        """
         if node == None:
             return L
         else:
