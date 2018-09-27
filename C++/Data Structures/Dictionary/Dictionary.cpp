624ea8c4a1059991441d865b59adfdce6670cbc5
Arturo Castillo <acv629@gmail.com>
2018-09-26 20:20:39 -0500

    Completed implementation, started tests

--- C++/Data Structures/Dictionary/Dictionary.cpp
+++ C++/Data Structures/Dictionary/Dictionary.cpp
@@ -1,5 +1,7 @@
 #include "Dictionary.h"
 #include <limits>
+#include <utility>
+#include <algorithm>
 #include <cstdlib>
 
 template<typename K, typename T>
@@ -16,6 +18,43 @@ ds::Dictionary<K,T>::Dictionary(){
   current_table = new vector<KeyValue>[table_size];
   shadow_table = new vector<KeyValue>[table_size * 2];
   srand(time(nullptr));
+}
+
+template<typename K, typename T>
+bool ds::Dictionary<K,T>::add(K key, T value){
+  if(get(key) != nullptr) return false;
+  auto key_value = std::make_pair(key, value);
+  if(object_count + 1 == table_size) resize();
+  addToTable(current_table, key_value, hash(key, dimension));
+  addToTable(shadow_table, key_value, hash(key, dimension+1));
+  return true;
+}
+
+template<typename K, typename T>
+T* ds::Dictionary<K,T>::get(const K& key){
+  int bucket = hash(key, dimension);
+  for(auto& k_v_pair:current_table[bucket]){
+    if(k_v_pair.first == key) return &k_v_pair.second;
+  }
+  return nullptr;
+}
+
+template<typename K, typename T>
+bool ds::Dictionary<K,T>::erase(const K& key){
+  int bucket = hash(key, dimension);
+  int shadow_bucket = hash(key, dimension + 1);
+  auto it =
+  std::remove_if(current_table[bucket].begin(),
+                  current_table[bucket].end(),
+                  [&](KeyValue& k_v_pair){
+                    return k_v_pair.first == key;
+                  });
+  std::remove_if(shadow_table[shadow_bucket].begin(),
+                  shadow_table[shadow_bucket].end(),
+                  [&](KeyValue& k_v_pair){
+                    return k_v_pair.first == key;
+                  });
+  return it != current_table.end();
 }
 
 template<typename K, typename T>
