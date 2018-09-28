b887bdc5b9cfa4c0c2658ecbfee59699e04d2a50
Miguel Concha <wormb0t@Miguels-MacBook-Pro.local>
2018-09-27 23:42:54 -0500

    Commenting Min and Max Heaps

--- Python/Data Structures/Heaps/MaxHeap.py
+++ Python/Data Structures/Heaps/MaxHeap.py
@@ -3,6 +3,16 @@
 from Heap import Heap
 
 class MaxHeap(Heap):
-
+	"""
+	Implements MaxHeaps
+	"""
     def __init__(self, L=[]):
+    	"""
+    	We construct a Heap with the max-ordering
+    	property from a list which if not defined is 
+    	to be empty.
+
+    	:param L: The list from which we construct the
+    	max-heap.
+    	"""
         Heap.__init__(self, False, L)
--- Python/Data Structures/Heaps/MinHeap.py
+++ Python/Data Structures/Heaps/MinHeap.py
@@ -3,6 +3,16 @@
 from Heap import Heap
 
 class MinHeap(Heap):
-
+	"""
+	Implements MinHeaps
+	"""
     def __init__(self, L=[]):
+    	"""
+    	We construct a Heap with the min-ordering
+    	property from a list which if not defined is 
+    	to be empty.
+
+    	:param L: The list from which we construct the
+    	min-heap.
+    	"""
         Heap.__init__(self, True, L)
